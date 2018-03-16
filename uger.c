c3d0daec9c40a31584aaf32379e953fdd60982dd
you1778 <you1778@inven.com>
2018-03-16 15:55:37 +0900

    first 윽거

---
+++ uger.c
@@ -0,0 +1,19 @@
+/*
+ * uger.c
+ *
+ *  Created on: 2018. 2. 26.
+ *      Author: you1778
+ */
+
+
+#include <stdio.h>
+
+int main(void)
+{
+
+	printf("hello\n");
+	printf("world.\n");
+
+	return 0;
+
+}
